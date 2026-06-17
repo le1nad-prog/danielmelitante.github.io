a7c1d033137fc225ad108c633b7381bd9192dc84
le1nad-prog <melitantedaniel1215@gmail.com>
2026-06-17 22:34:05 +0800

    Portfolio Creation: Implement JIT Content Rendering For Modular Pages

---
+++ assets/js/navigation.js
@@ -0,0 +1,32 @@
+function clearActiveNavButtons() {
+
+    const navLinks = document.getElementsByClassName('nav-link');
+
+    Array.from(navLinks).forEach(link => {
+        link.classList.remove('active');
+    });
+}
+
+function setActiveNavButton(buttonName) {
+
+    const navButtons = document.getElementsByClassName(buttonName);
+
+    Array.from(navButtons).forEach(button => {
+        button.classList.add('active');
+    });
+}
+
+function loadPage(content = 'home', buttonName = 'homeButton') {
+
+    clearActiveNavButtons();
+    setActiveNavButton(buttonName);
+
+    const contents = document.getElementById('contents');
+
+    fetch(`content/${content}.html`)
+        .then(response => response.text())
+        .then(data => {
+        contents.innerHTML = data;
+        })
+        .catch(error => console.error('Error fetching file:', error));
+}
